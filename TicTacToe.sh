e31b93fca1d699ba6eee45aa37d3a5ef3c882a6f
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-11 19:25:39 +0530

    ADDED : check won or tie or change turn

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -12,33 +12,38 @@ function resetGameboard()
 		done
 	}
 resetGameboard
-function checkLetter()
-	{
-		if [ $((RANDOM%2)) == 1 ]
-		then
-			letter="X"
-		else
-			letter="o"
-		fi
-	}
-checkLetter
+
 function checkToss()
 	{
 		toss=$((RANDOM%2))
 		if [ $toss == 1 ]
 		then
-			echo "player 1 first play "
+			player="player-1"
+			letter="o"
 		else
-			echo "player 2 first play "
+			player="player-2"
+			letter="x"
 		fi
+		chooseCell $player $letter
 	}
-checkToss
+function playerturn()
+	{
+		if [ $player == "player-1" ]
+		then
+			player="player-2"
+			letter="x"
+		else
+			player="player-1"
+			letter="o"
+		fi
+		chooseCell $player $letter
+	}
 function chooseCell()
 	{
 		num=0
 		while [[ $num -lt 10 ]]
 		do
-			read -p "choose any position on board " position
+			read -p "$player : choose any position on board " position
 			for ((i=0;i<9;i++))
 			do
 				if [[ ${gameboard[$i]} -eq $position ]]
@@ -46,8 +51,61 @@ function chooseCell()
 					gameboard[$i]=$letter
 				fi
 			done
-		resetGameboard
-		((num=$num+1))
+			resetGameboard
+			((num=$num+1))
+
+			checkwinner $letter
+
+			if [[ $win == 1 ]]
+			then
+				echo "$player win"
+				break;
+			fi
+			playerturn $player
 		done
 	}
-chooseCell
+function checkwinner()
+	{
+		input=$1
+		symbol=$input$input$input
+		arrangement=" "
+		#check rows for winning condition
+		for ((i=0;i<9;i=$i+3))
+      do
+      	arrangement=${gameboard[$i]}${gameboard[$i+1]}${gameboard[$i+2]}
+			if [ $arrangement == $symbol ]
+			then
+				win=1
+				echo $win
+			fi
+		done
+		#check columns for winning condition
+		arrangement=" "
+		for ((i=0;i<3;i++))
+      do
+         arrangement=${gameboard[$i]}${gameboard[$i+3]}${gameboard[$i+6]}
+         if [ $arrangement == $symbol ]
+         then
+            win=1
+            echo $win
+         fi
+      done
+		#check diagonal for winning condition
+		arrangement=" "
+		arrangement=${gameboard[0]}${gameboard[4]}${gameboard[8]}
+		if [ $arrangement == $symbol ]
+		then 
+			win=1
+			echo $win
+		fi
+		arrangement=" "
+      arrangement=${gameboard[2]}${gameboard[4]}${gameboard[6]}
+      if [ $arrangement == $symbol ]
+      then 
+         win=1
+         echo $win
+      fi
+
+	}
+
+checkToss
