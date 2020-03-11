2f91a87a76ee1b3160c256d9e232cee58f58ee36
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-11 15:19:52 +0530

    ADDED : resetting game board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,11 @@
-#!/bin/bash -x 
+#!/bin/bash -x
 echo "Wel-Come To Tic-Tac-Toe Game ..!"
+
+declare -a gameboard
+gameboard=(1 2 3 4 5 6 7 8 9)
+echo "------------"
+for ((i=0;i<9;i=$i+3))
+do
+	echo " ${gameboard[$i]}  ""|""${gameboard[$i+1]}  ""|""${gameboard[$i+2]} "
+	echo "------------"
+done
