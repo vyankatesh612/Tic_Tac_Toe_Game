2d7a6d9c609e8b053307a5e475f827254f753447
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-11 15:27:43 +0530

    ADDED : check letter is X or 0

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -x
 echo "Wel-Come To Tic-Tac-Toe Game ..!"
-
 declare -a gameboard
 gameboard=(1 2 3 4 5 6 7 8 9)
-echo "------------"
-for ((i=0;i<9;i=$i+3))
-do
-	echo " ${gameboard[$i]}  ""|""${gameboard[$i+1]}  ""|""${gameboard[$i+2]} "
-	echo "------------"
-done
+function resetGameboard()
+	{
+		echo "------------"
+		for ((i=0;i<9;i=$i+3))
+		do
+			echo " ${gameboard[$i]}  ""|""${gameboard[$i+1]}  ""|""${gameboard[$i+2]} "
+			echo "------------"
+		done
+	}
+resetGameboard
+function checkLetter()
+	{
+		if [ $((RANDOM%2)) == 1 ]
+		then
+			letter="X"
+		else
+			letter="0"
+		fi
+}
+checkLetter
